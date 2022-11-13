e768aa0559519f5075336e2ca467a5661466ea4e
BCSF19A033-Adnan <116968748+BCSF19A033-Adnan@users.noreply.github.com>
2022-11-13 20:36:01 +0500

    Add question/ans list

--- ExamApp/app/src/main/java/com/example/examapp/MainActivity.java
+++ ExamApp/app/src/main/java/com/example/examapp/MainActivity.java
@@ -12,12 +12,19 @@ public class MainActivity extends AppCompatActivity {
 
     TextView questionNo, question;
     Button option1, option2, option3, option4;
-    String [] questionList = {"d", "h", "l", "k", "t",
-            "b", "f", "", "", ""};
-    String [][] optionsForQuestions = {{}, {}, {}, {}, {},
-            {}, {}, {}, {}, {}};
-    String [] correctAnsList = {"d", "h", "l", "k", "t",
-            "b", "f", "", "", ""};
+    String [] questionList = {"Which was the king who established rationing system?", "Who robbed the Arabs trade ship?",
+            "How many times did Mahmood Ghaznavi invade on India?", "From where did the Aryans come into South Asia?",
+            "What was called the head of Shaqq?", "What was called the head of the Diwan-e-Arz?",
+            "Whose daughter was Razia Sultan?", "Who were defeated by the Aryans in South Asia?",
+            "Which was the religion, whose founder was Mahaveer?", "From which race Khiljis belonged?"};
+
+    String [][] optionsForQuestions = {{"Qutubuddin Aibak", "Illtutmish", "Allauddin", "Muhammad Tughlaq"}, {"Makranis", "Robbers", "Pirates", "Fisher men"}, {"Fifteen", "Sixteen", "Seventeen", "Eighteen"},
+            {"Central Asia", "Middle East", "Greece", "China"}, {"Shiqq-Dar", "Soob-e-Dar", "Nazim", "Kotwal"}, {"Sadr-us-sadr", "Arz", "Sahib-e-Diwan", "Wazir"},
+            {"Qutubuddin Aibak", "Sultan Ghias ud Din", "Sultan Iltutmish", "Nasir ud Din"}, {"Sumerians", "Turks", "Dravidians", "Chinese"}, {"Hinduism", "Jainism", "Sikhism", "Buddhism"},
+            {"Turk", "Afghan", "Arab", "Mughal"}};
+
+    String [] correctAnsList = {"Allauddin", "Pirates", "Seventeen", "Central Asia", "Shiqq-Dar",
+            "Arz", "Sultan Iltutmish", "Dravidians", "Jainism", "Turk"};
     ArrayList<Integer> wrongAnsList = new ArrayList<>();
     int noOfWrongQuestions;
 
